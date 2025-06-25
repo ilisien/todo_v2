a5da79786550b7bba470fe1af6986eae45693707
ilisien <ian8lisien@gmail.com>
2025-06-25 10:49:19 -0400

    placeholder color names

--- client/src/PrefsModal.jsx
+++ client/src/PrefsModal.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 
 const PrefsModal = ({ isOpen, onClose }) => {
-    const [bkgColor, setBkgColor] = useState('#242424')
-    const [txtColor, setTxtColor] = useState('#ffffff')
-    const [lnkColor, setLnkColor] = useState('#646cff')
-    const [errColor, setErrColor] = useState('#')
+    const [bkgColor, setBkgColor] = useState('#242424');
+    const [txtColor, setTxtColor] = useState('#ffffff');
+    const [lnkColor, setLnkColor] = useState('#646cff');
+    const [wrnColor, setWrnColor] = useState('#000000');
+    const [errColor, setErrColor] = useState('#000000');
+    const [scsColor, setScsColor] = useState('#000000');
 }
